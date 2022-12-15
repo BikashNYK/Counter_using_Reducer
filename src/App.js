a92d5db1673d04cb557a778f6281adbe42fbfa66
Bikash Kumar Nayak <fluxtrn@gmail.com>
2022-12-15 21:41:49 +0530

    Counter_using_Reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { useReducer } from 'react';
+const initialTaskState ={
+  count:0
+}
+const reducer = (state,action)=>{
+  if(action.type === 'add'){
+    return state +1
+  }
+  if (action.type === 'delete') {
+    return state  - 1;
+  }
+  if (action.type === 'deleteAll') {
+    return state  = 0;
+  }
+  return state 
+}
 function App() {
+  const [taskState , dispatch] = useReducer(reducer, initialTaskState.count)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Task Counter</h2>
+      <button onClick={() => dispatch({ type: 'add' })} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({ type: 'delete' })} id="delTaskBtn">Delete a task</button>
+      <button onClick={() => dispatch({ type: 'deleteAll' })} id="delAllTaskBtn">Delete all tasks</button>
+      <h3 className="counter">Number of Tasks : {taskState }</h3>
     </div>
   );
 }
